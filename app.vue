7b1b457bc387005aaab150585ce20bdb7ae40919
Paul Steuber <43008359+paulsteuber@users.noreply.github.com>
2024-01-10 21:40:54 +0100

    ssr test

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <h1>{{ ssr }}</h1>
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+import { ref }  from 'vue';
+const ssr = ref("Nö");
+if (!process.client){
+  ssr.value = "Juhuhuuuuu"
+}
+</script>
